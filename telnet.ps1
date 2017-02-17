6cebac4b4a90fae0c77d647667cc463df2a4766c
Marcus J <marcusjo@users.noreply.github.com>
2017-02-17 14:56:53 -0600

    Port testing tool for W7
    For machines without telnet client enabled and no Test-NetConnection ps function.

---
+++ telnet.ps1
@@ -0,0 +1,16 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True,Position=1)]
+   [string]$ip,
+
+   [Parameter(Mandatory=$True,Position=2)]
+   [int]$port
+)
+
+$connection = New-Object System.Net.Sockets.TcpClient($ip, $port)
+if ($connection.Connected) {
+    Return "Connection Success"
+}
+else {
+    Return "Connection Failed"
+}
